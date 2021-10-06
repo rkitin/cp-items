61db0b23aec4a813d4583e97a3e515028e439277
Cherukuri Rajendra Kumar <rcherukuri_sta@ku.edu>
2021-10-06 18:04:44 +0530

    restructured

--- src/components/create/multiplechoice/index.js
+++ src/components/create/multiplechoice/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './index.css'
 
 const MultipleChoice = ({ content, onUpdate, itemType }) => {
-  return <div className={styles.test_mc}>Example Component: text</div>
+  return <div className={styles.test_mc}>Example Component: asdfkasjdl </div>
 }
 
 export default MultipleChoice
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './styles.module.css'
-import MultipleChoice from './components/multiplechoice'
+import MultipleChoice from './components/create/multiplechoice'
 
 const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
