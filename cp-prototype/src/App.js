ff88aea1ff0e0076909b66c0e6eed014bcde2e80
Cherukuri Rajendra Kumar <rcherukuri_sta@ku.edu>
2021-10-06 22:17:53 +0530

    Added store

--- cp-prototype/src/App.js
+++ cp-prototype/src/App.js
@@ -2,14 +2,26 @@ import React from 'react'
 
 import { MultipleChoice, MultipleChoicePreview } from 'cp-items'
 import 'cp-items/dist/index.css'
+import { useSelector } from 'react-redux'
+import DOMPurify from 'dompurify'
 
 const App = () => {
+  const item = useSelector((store) => store.item)
+
   return (
     <>
-      <MultipleChoice />
-      
-      <div>Preview : </div>
+      <span>
+        {DOMPurify.sanitize(JSON.stringify(item.name), {
+          USE_PROFILES: { html: true }
+        })}{' '}
+      </span>
 
+      <div>Create: </div>
+      <br />
+      <MultipleChoice />
+
+      <div>Preview : </div>
+      <br />
       <MultipleChoicePreview />
 
     </>
