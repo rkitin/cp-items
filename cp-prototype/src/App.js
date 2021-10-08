fbf88165b63230f3bdca0a374bbcec7c0dabdc3a
Cherukuri Rajendra Kumar <rcherukuri_sta@ku.edu>
2021-10-08 16:34:02 +0530

    Renamed component

--- cp-prototype/src/App.js
+++ cp-prototype/src/App.js
@@ -1,21 +1,12 @@
-import React from 'react'
+import { Fragment, default as React } from 'react'
 
 import { MultipleChoice, MultipleChoicePreview } from 'cp-items'
 import 'cp-items/dist/index.css'
-import { useSelector } from 'react-redux'
-import DOMPurify from 'dompurify'
 
 const App = () => {
-  const item = useSelector((store) => store.item)
 
   return (
     <>
-      <span>
-        {DOMPurify.sanitize(JSON.stringify(item.name), {
-          USE_PROFILES: { html: true }
-        })}{' '}
-      </span>
-
       <div>Create: </div>
       <br />
       <MultipleChoice />
